1b30c5bd287c68b635f69852b802fcb9e806c649
Matthias J. Kannwischer <matthias@kannwischer.eu>
2025-02-26 11:54:05 +0800

    Extended API: Adjust CBMC proofs
    Signed-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>

--- mlkem/kem.c
+++ mlkem/kem.c
@@ -288,7 +288,6 @@ int crypto_kem_keypair_derand_struct(mlk_public_key *pk, mlk_secret_key *sk,
   /* Declassify public key */
   MLK_CT_TESTING_DECLASSIFY(&pks->indcpa_pk, sizeof(mlk_indcpa_public_key));
 
-
   /* Specification: Partially implements
    * [FIPS 203, Section 3.3, Destruction of intermediate values] */
   mlk_zeroize(pks, sizeof(pks));
@@ -519,6 +518,8 @@ int crypto_kem_dec(uint8_t ss[MLKEM_SSBYTES],
   }
 
   res = crypto_kem_dec_struct(ss, ct, &sks);
+  /* Specification: Partially implements
+   * [FIPS 203, Section 3.3, Destruction of intermediate values] */
   mlk_zeroize(&sks, sizeof(mlk_secret_key));
   return res;
 }
@@ -528,7 +529,13 @@ MLK_MUST_CHECK_RETURN_VALUE
 int crypto_kem_sk_from_seed(mlk_secret_key *sk,
                             const uint8_t coins[2 * MLKEM_SYMBYTES])
 {
-  return crypto_kem_keypair_derand_struct(NULL, sk, coins);
+  mlk_public_key pk;
+  int ret;
+  ret = crypto_kem_keypair_derand_struct(&pk, sk, coins);
+  /* Specification: Partially implements
+   * [FIPS 203, Section 3.3, Destruction of intermediate values] */
+  mlk_zeroize(&pk, sizeof(mlk_public_key));
+  return ret;
 }
 
 MLK_EXTERNAL_API
